65912a11cbbd89fc331efa08ed5c213c33e7a1a4
Oliver Dudman <oliver.dudman@icloud.com>
2018-06-19 14:34:45 +0100

    survey form initial commit

---
+++ build_a_survey_form/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const reload = browserSync.reload;
+const sass = require("gulp-ruby-sass");
+
+gulp.task("sass", function() {
+  return sass("scss/main.scss")
+  .pipe(gulp.dest("css"))
+  .pipe(reload({stream: true}));
+});
+
+gulp.task("serve", ["sass"], function() {
+  browserSync({
+    server: true,
+  });
+
+  gulp.watch(["scss/*.scss"], ["sass"]);
+  gulp.watch(["index.html"], reload);
+});
