cf2e06cf6d59860894b16effc249c79bb98eabb2
Oliver Dudman <oliver.dudman@icloud.com>
2018-06-24 22:21:57 +0100

    add click listeners to nav items

--- build_a_product_landing_page/js/main.js
+++ build_a_product_landing_page/js/main.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   var toggler = document.getElementById("nav__toggler");
   var header = document.getElementById("header");
+  var links = document.getElementsByClassName("nav-link");
+
   toggler.addEventListener("click", function() {
     header.classList.toggle("active");
   });
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+      header.classList.toggle("active");
+    });
+  }
 });
